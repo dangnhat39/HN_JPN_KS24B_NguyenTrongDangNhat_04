99594b42c98be0d71c432c25633621a1411d6839
dangnhat39 <nhathat111@gmail.com>
2025-01-07 11:12:43 +0700

    Update userFunctions.h

--- userFunctions.h
+++ userFunctions.h
@@ -1,18 +1,6 @@
 #ifndef USERFUNCTIONS_H  
 #define USERFUNCTIONS_H  
-
-typedef struct {  
-    char userNumber[11];  
-    char name[50];  
-    char email[50];  
-    char phone[15];  
-    int gender;  
-    int day, month, year;  
-    char username[20];  
-    double balance;  
-    char status[10];
-    char input[100];  
-} User;  
+#include "datatypes.h"
 
 void addUser();  
 void showAllUsers();  
@@ -25,3 +13,4 @@ void showMainMenu();
 void showUserLoginScreen();  
 void saveUsersToBinaryFile(const char *filename);
 #endif
+
